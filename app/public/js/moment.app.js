18c250753f991c33b75f6d5900159fb2f6cc70ec
Alexis Yannuzzi <yannuzzi367@gmail.com>
2020-09-16 21:33:15 -0400

    Completed the moment.app.js file

---
+++ app/public/js/moment.app.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el: '#momentApp',
+  data:{
+    someDate: '1993-07-20T09:44:18.674Z'
+  },
+  computed: {
+    age() {
+      return moment().diff(this.someDate, 'years');
+    }
+  },
+  methods:{
+    formatDate(d) {
+      return moment(d).format("dddd, MMMM Do YYYY");
+    }
+  },
+
+})
