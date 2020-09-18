3abdce95e7cd7b2740806640fa0cf994f7ee9d67
Alexis Yannuzzi <yannuzzi367@gmail.com>
2020-09-18 18:31:26 -0400

    got the email link to work

--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -1,16 +1,41 @@
 var app = new Vue({
   el: '#vue-app',
   data: {
-    user:'',
-  },
+  user: {
+    "results":[{
+      "name":{
+        "first":"",
+        "last": ""
+      },
+      "email":"",
+      "picture": {
+        "medium":""
+      },
+      "location":{
+        "country": ""
+      },
+      "dob": {
+        "date": ""
+      }
+        }]
+  }
+},
 
   created() {
       fetch('https://randomuser.me/api/')
         .then(response => response.json())
         .then(json =>
           {this.user = json;
-
           console.log(json);}
-        );
+        )
+        .catch((error)=>{
+          console.error('Error:', error);
+          fetch('./JSON/randomuser.me-sample.json')
+          .then(response => response.json())
+          .then(json =>{
+            {this.user = json;
+            console.log(json);}
+          })
+        });
          }
        });
